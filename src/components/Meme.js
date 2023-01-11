0b23a68fa2e2f26c434cbc964dd0f207f00b994d
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-10 19:33:17 -0800

    Add text inputs

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,6 +2,18 @@ export default function Meme() {
   return (
     <main>
       <div className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          name="topText"
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          name="bottomText"
+        />
       </div>
     </main>
   );
