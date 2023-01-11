7c13d74532d38d100c5ac0dda732f8b6cbe228e7
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-10 19:34:17 -0800

    Update Meme.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -18,6 +18,11 @@ export default function Meme() {
           Get a new meme image 🖼
         </button>
       </div>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" alt="meme" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
