de5daa1fa3e6301e7e9d757d0b52bff37ac03303
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-10 19:34:38 -0800

    Add state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,4 +1,12 @@
+import React, { useState, useEffect } from "react";
+
 export default function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemes, setAllMemes] = useState([]);
   return (
     <main>
       <div className="form">
