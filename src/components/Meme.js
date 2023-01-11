8d943066ed98d034eb85601d60ea45ed64fcd85d
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-10 19:35:08 -0800

    Add getMemeImage function

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -13,6 +13,15 @@ export default function Meme() {
       .then((res) => res.json())
       .then((memesData) => setAllMemes(memesData.data.memes));
   }, []);
+
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
   return (
     <main>
       <div className="form">
